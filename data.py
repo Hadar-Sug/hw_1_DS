0ee54b5b18c9e1a2a37bce8baba931eaae70029a
Hadar-sug <Hadar.sug@campus.technion.ac.il>
2022-04-08 12:18:25 +0300

    finished last func

--- data.py
+++ data.py
@@ -4,6 +4,12 @@ import statistics as stat
 
 
 def load_data(path, features):
+    """
+    forming a dictionary of the data
+    :param path: path to the data
+    :param features: guidelines features in data
+    :return: dictionary of the dataframe
+    """
     df = pandas.read_csv(path)
     data = df.to_dict(orient="list")
     features = string_to_array(features)
@@ -11,12 +17,24 @@ def load_data(path, features):
 
 
 def num_of_row(feature, data):
+    """
+    finds the row of feature
+    :param feature: the feature we want the row of
+    :param data: the dataframe
+    :return: num of row
+    """
     for k in range(5):
         if data[k][0] == feature:
             return k
 
 
 def matrix_to_dict(matrix, data):
+    """
+    forming a dictionary from a given matrix
+    :param matrix: data including matrix
+    :param data: the data
+    :return: the dataframe
+    """
     dictionary = dict()
     for index, item in enumerate(data):
         feature = item[0]
@@ -26,10 +44,24 @@ def matrix_to_dict(matrix, data):
 
 
 def string_to_array(string):
+    """
+    getting rid of commas
+    :param string: sting were filtering
+    :return: returns said string as an array without commas
+    """
     return string.split(',')
 
 
 def filter_by_feature(data, feature, value):
+    """
+    gets a dictionary, splits it based on a categorical value, to 2 dictionaries
+    :param data: the dataframe
+    :param feature: the categorical feature
+    :param value: value of feature, based on this value we split the dict
+    :return:
+    data1 is a dictionary with values that correspond to value
+    data2 is another dict with the rest
+    """
     data_all = np.array([data[k][1] for k in range(5)])
     data1 = np.array([])
     data2 = np.array([])
@@ -45,6 +77,12 @@ def filter_by_feature(data, feature, value):
 
 # need to change functions to for loops
 def print_details(data, features, statistic_functions):
+    """
+    prints statistic measures about the dataframe acc to features, using statistics.py methods
+    :param data: the data frame
+    :param features: the wanted features to base on
+    :param statistic_functions: a list concluding statistic methods
+    """
     for feature in features:
         print("{}: ".format(feature))
         for i, func in enumerate(statistic_functions):
@@ -57,6 +95,13 @@ def print_details(data, features, statistic_functions):
 
 
 def print_joint_details(data, features, statistic_functions, statistic_functions_names):
+    """
+    prints statistic measures about the dataframe, only based on features
+    :param data: the dataframe
+    :param features: the features were calculating according to
+    :param statistic_functions: list of statistic functions
+    :param statistic_functions_names: list of the names of the functions
+    """
     feature1 = data[features[0]]
     feature2 = data[features[1]]
     for i, func in enumerate(statistic_functions):
@@ -65,17 +110,23 @@ def print_joint_details(data, features, statistic_functions, statistic_functions
         print("{}({}, {}): {}".format(method_name, features[0], features[1], function))
 
 
-def filter_by_feature_hadar(data, feature, value):
-    true_dict = {}
-    false_dict = {}
-    for k, v in data:  # iterate over our data
-        true_list = []  # whichever lines match our condtion of 'value', well add to this list
-        false_list = []  # the rest go here
-        for a, wanted in zip(v, data[feature].values()): # iterate over the wanted feature list and one of the columns
-            if wanted == value:
-                true_list.append(a)
-            else:
-                false_list.append(a)
-        true_dict[k] = true_list
-        false_dict[k] = false_list
-    return true_dict, false_dict
+# def filter_by_feature_hadar(data, feature, value):
+#     """
+#     :param data:
+#     :param feature:
+#     :param value:
+#     :return:
+#     """
+#     true_dict = {}
+#     false_dict = {}
+#     for k, v in data:  # iterate over our data
+#         true_list = []  # whichever lines match our condtion of 'value', well add to this list
+#         false_list = []  # the rest go here
+#         for a, wanted in zip(v, data[feature].values()): # iterate over the wanted feature list and one of the columns
+#             if wanted == value:
+#                 true_list.append(a)
+#             else:
+#                 false_list.append(a)
+#         true_dict[k] = true_list
+#         false_dict[k] = false_list
+#     return true_dict, false_dict
--- statistics.py
+++ statistics.py
@@ -1,14 +1,27 @@
 import math
 
+import data
+
 
 def calc_mean(values):
+    """
+    calculates the mean
+    :param values: calculating the mean of these values
+    :return: mean
+    """
     sum = 0
     for value in values:
         sum += value
+
     return sum / len(values)
 
 
 def calc_stdv(values):
+    """
+    calculates the standard deviation
+    :param values: calculating the standard deviation of these values
+    :return: standard deviation
+    """
     sum = 0
     mean = calc_mean(values)
     for value in values:
@@ -17,9 +30,28 @@ def calc_stdv(values):
 
 
 def calc_covariance(values1, values2):
+    """
+    calculates the covariance of 2 values
+    :param values1:first list of values to be used in calculating
+    :param values2:second list of values to be used in calculating
+    :return:covariance
+    """
     sum = 0
     mean1 = calc_mean(values1)
     mean2 = calc_mean(values2)
     for value1, value2 in zip(values1, values2):
         sum += (value1 - mean1) * (value2 - mean2)
+
     return sum / (len(values1) - 1)
+
+
+def population_statistics(feature_description, data, treatment, target, threshold, is_above, statistic_functions):
+    data["flag"] = []
+    for temp in data[treatment]:
+        if temp > threshold:
+            data["flag"].append(is_above)
+        else:
+            data["flag"].append(not is_above)
+    wanted, not_wanted = data.filter_by_feature(data, "flag", is_above)
+    print(f"{feature_description}:\n")
+    data.print_details(wanted, target, statistic_functions)
